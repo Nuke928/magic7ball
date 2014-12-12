fc1bca35421260ae4c3246640bcd342233589bc9
Gian Sass <gian.sass@outlook.de>
2014-12-12 15:46:53 +0000

    Update build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,10 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    jsDir: 'public/js/',
+    jsDistDir: 'dist/js/',    
+    cssDir: 'public/css/',
+    cssDistDir: 'dist/css/',
+    pkg: grunt.file.readJSON('package.json'),
     jshint: {
       all: [
         '.'
@@ -15,11 +20,95 @@ module.exports = function(grunt) {
       },
 
       all: { src: 'tests/**/*.js' }
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'dist/css/global.css': 'public/css/global.sass'
+        }
+      }
+    },
+    concat: {
+      js: {
+        options: {
+          separator: ';'
+        },
+        src: ['<%=jsDir%>*.js'],
+        dest: '<%=jsDistDir%><%= pkg.name %>.js'
+      },
+      css: {
+        src: ['<%=cssDir%>*.css','dist/css/global.css'],
+        dest: '<%=cssDistDir%><%= pkg.name %>.css'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%=grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          '<%=jsDistDir%><%= pkg.name %>.min.js': ['<%= concat.js.dest %>']
+        }
+      }
+    },
+    cssmin: {
+      add_banner: {
+        options: {
+          banner: '/*! <%= pkg.name %> <%=grunt.template.today("dd-mm-yyyy") %> */\n'
+        },
+        files: {
+          '<%=cssDistDir%><%= pkg.name %>.min.css': ['<%= concat.css.dest %>']
+        }
+      }
+    },
+    watch: {
+      files: ['<%=jsDir%>*.js', '<%=cssDir%>*.css', '<%=cssDir%>*.sass'],
+      tasks: ['sass', 'concat', 'uglify', 'cssmin', 'clean:postbuild']
+    },
+    imagemin: {
+        dynamic: {
+            files: [{
+                expand: true,
+                cwd: 'public/images/',
+                src: ['**/*.{png,jpg,gif}'],
+                dest: 'dist/images/'
+            }]
+        }
+    },
+    copy: {
+      favicon: {
+        files: [
+          { 
+            expand: true,
+            cwd: 'public/images/', 
+            src: ['favicon'], 
+            dest:'dist/images/' 
+          }
+        ]
+      }
+    },
+    clean: {
+      postbuild: {
+        src: ['dist/css/global.css']
+      }
     }
   });
-  
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-clean');
   
-  grunt.registerTask('default', ['simplemocha','jshint']);
+  grunt.registerTask('build', ['sass', 'concat', 'uglify', 'cssmin', 'imagemin', 'copy', 'clean:postbuild']);
+  grunt.registerTask('test', ['jshint', 'simplemocha']);
+  grunt.registerTask('default', ['test', 'build', 'watch']);
 };
